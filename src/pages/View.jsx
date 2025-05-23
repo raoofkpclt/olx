07032cc21e1f1e3f9ce5e620b132a81109e47c31
Abdul Raoof KP <raoofa109@gmail.com>
2025-05-24 00:07:59 +0530

    Update View.jsx

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -167,7 +167,6 @@ const ProductView = () => {
                   Report this ad
                 </button>
               </div>
-              <Footer />
             </div>
           </div>
         </div>
